4d056c36705240ab0038a66b9fdb1fdc402e2c81
jhenrique09 <jhsv09@gmail.com>
2023-08-09 22:29:23 +0000

    vendor: Drop SimpleDeviceConfig
    Change-Id: Iba7e9e042b037f1147647608de9ab03a21fa00f9

--- config/common.mk
+++ config/common.mk
@@ -86,10 +86,6 @@ PRODUCT_COPY_FILES += \
     vendor/ultimate/prebuilt/common/etc/init/init.ultimate-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.ultimate-updater.rc
 endif
 
-# Config
-PRODUCT_PACKAGES += \
-    SimpleDeviceConfig 
-
 # Disable RescueParty due to high risk of data loss
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true
