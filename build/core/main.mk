503e56c15d0bee9154801c825e9455754dd33c18
RITESH <ultimateritesh987@gmail.com>
2024-08-04 15:36:50 +0530

    vendor: Initial rebrand for UltimateOS Signed-off-by: RITESH <ultimateritesh987@gmail.com>

--- build/core/main.mk
+++ build/core/main.mk
@@ -1,2 +1,2 @@
-# Include LineageOS versions
-include $(TOPDIR)vendor/lineage/build/core/main_version.mk
+# Include UltimateOS versions
+include $(TOPDIR)vendor/ultimate/build/core/main_version.mk
--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -4,22 +4,13 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
-# LineageOS System Version
+# UltimateOS System Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
+    ro.ultimate.version=$(ULTIMATE_VERSION) \
+    ro.ultimate.releasetype=$(ULTIMATE_BUILDTYPE) \
+    ro.ultimate.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(ULTIMATE_VERSION)
 
-# LineageOS Platform Display Version
+# UltimateOS Platform Display Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
-
-# LineageOS Platform SDK Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
-
-# LineageOS Platform Internal Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
+    ro.ultimate.display.version=$(ULTIMATE_DISPLAY_VERSION)
