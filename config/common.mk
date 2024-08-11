635c8c12406fc4e66d6d3f91795078259395aa14
RITESH <ultimateritesh987@gmail.com>
2024-08-12 00:32:02 +0530

    vendor: Inherit GMS Services
    Signed-off-by: RITESH <ultimateritesh987@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -269,6 +269,9 @@ CUSTOM_LOCALES += \
 # Bootanimation
 $(call inherit-product, vendor/ultimate/config/bootanimation.mk)
 
+# Inherit GMS Services
+$(call inherit-product, vendor/gms/products/gms.mk)
+
 # Fonts
 include vendor/ultimate/config/fonts.mk
 
